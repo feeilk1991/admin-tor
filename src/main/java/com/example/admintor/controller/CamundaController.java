5ca1ecb8f8aa4cbf6faef44421c6cc91206975b1
romanov <romanov@evolenta.ru>
2023-05-04 16:56:23 +0300

    init next

--- src/main/java/com/example/admintor/controller/CamundaController.java
+++ src/main/java/com/example/admintor/controller/CamundaController.java
@@ -1,11 +1,20 @@
 package com.example.admintor.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
 public class CamundaController {
-    //http://camunda.torknd.cloud-uat.vms.torcloud.digital.gov.ru/rest/process-instance/
+    private final String PROD = "http://camunda.torknd.cloud-uat.vms.torcloud.digital.gov.ru";
 
+    private final String DEMO = "http://camunda.knd.cloud-preprod.vms.torcloud.digital.gov.ru/";
+    @GetMapping(DEMO)
+    public void  getInstance() {
+        return ;
+    }
+
+    @GetMapping(PROD)
+    public void  getInstance1() {
+        return ;
+    }
 }
