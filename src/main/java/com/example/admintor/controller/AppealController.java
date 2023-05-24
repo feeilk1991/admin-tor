48f8451e30f6a3debd9fc6df86647de41fb07632
romanov <romanov@evolenta.ru>
2023-05-24 18:16:10 +0300

    create search

--- src/main/java/com/example/admintor/controller/AppealController.java
+++ src/main/java/com/example/admintor/controller/AppealController.java
@@ -7,7 +7,6 @@ import com.example.admintor.service.appeal.AppealService;
 import com.example.admintor.service.camunda.CamundaService;
 import com.example.admintor.service.passport.AppealPassportService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
@@ -31,44 +30,38 @@ public class AppealController {
     @GetMapping("/data")
     public ModelAndView showAllAppeals(Model model) throws NullPointerException {
         List<Appeal> appeals = new ArrayList<>();;
-
+        model.addAttribute("id", null);
         //TODO
-        Appeal appeal = getDataByIdFromSource("6385d4e4a978a3000137ba6e");
-        try {
-            appeals.add(appeal);
-        } catch (NullPointerException e) {
-            model.addAttribute("appeals", e.getMessage());
-        }
+//        Appeal appeal = getDataByIdFromSource("6385d4e4a978a3000137ba6e");
+//        try {
+//            appeals.add(appeal);
+//        } catch (NullPointerException e) {
+//            model.addAttribute("appeals", e.getMessage());
+//        }
 
         model.addAttribute("appeals", appeals);
 
         return new ModelAndView("data-view");
     }
 
-    /*@GetMapping("/data")
-    public ModelAndView showDataForm(Model model) {
-        model.addAttribute("id", null);
-        return new ModelAndView("data-view");
-    }*/
-
-    /*@GetMapping("/data/{id}")
+    @GetMapping("/data/{id}")
     public ModelAndView getDataById(String id, Model model) {
         Appeal appeal = getDataByIdFromSource(id);
+        List<Appeal> appeals = new ArrayList<>();
+        appeals.add(appeal);
+        model.addAttribute("appeals", appeals);
         model.addAttribute("appeal", appeal);
-        if (appeal != null) {
-            Camunda camunda = camundaService.getCamundaByMainId(id);
-            model.addAttribute("camunda", camunda);
-
-            AppealPassport appealPassport = appealPassportService.getAppealPassportId(id);
-            model.addAttribute("passport", appealPassport);
-
-        }
         return new ModelAndView("data-view");
-    }*/
+    }
 
     @GetMapping("/data/details/{id}")
     public ModelAndView getDataAppealId(@PathVariable("id")String id, Model model) {
         Appeal appeal = getDataByIdFromSource(id);
+        getAppealAttribute(appeal, model, id);
+        return new ModelAndView("data-details");
+    }
+
+    private void getAppealAttribute(Appeal appeal, Model model, String id) {
         model.addAttribute("appeal", appeal);
         if (appeal != null) {
             Camunda camunda = camundaService.getCamundaByMainId(id);
@@ -78,15 +71,10 @@ public class AppealController {
             model.addAttribute("passport", appealPassport);
 
         }
-        return new ModelAndView("data-details");
     }
 
     private Appeal getDataByIdFromSource(String id) {
         Optional<Appeal> myDataOptional = Optional.of(appealService.getAppealId(id));
         return myDataOptional.orElse(null);
-    }
-
-    private List<Appeal> getDataAppeals() {
-        return appealService.getListAppeals();
     }
 }
