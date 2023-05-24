40df391da2c09401baf625a4c8e66cd02e0ec98d
romanov <romanov@evolenta.ru>
2023-05-24 17:53:41 +0300

    init next

--- src/main/java/com/example/admintor/models/Appeal.java
+++ src/main/java/com/example/admintor/models/Appeal.java
@@ -4,11 +4,7 @@ import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
-
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 @Data
 @Document(collection = "appeals")
@@ -17,16 +13,8 @@ public class Appeal {
     private String id;
     private String number;
     private String shortNumber;
-
-    @Field("erpEnvelopesInfo")
-    private List<EnvelopesInfo> envelopesInfos;
-
-    @Field("subservices")
-    private Object subServices;
-
-    @Field("subservices[0].camundaProcessInfo")
-    private Map<Camunda, String> camundaProcessInfo;
-
-    @Field("subservices[0].camundaProcessInfo.camundaBusinessInfoId")
-    private String camundaBusinessInfoId;
+    @Field("parentEntries")
+    private String parentEntries;
+    @Field("guid")
+    private String guid;
 }
